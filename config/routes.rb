caebd7a9541217fd28b2ac447209c49806b5e92a
Sam <Samuel.g.grimes@gmail.com>
2014-01-06 12:55:07 -0600

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Runnery::Application.routes.draw do
  root 'static_pages#home'
  match '/help',  to: 'static_pages#help',  via: 'get'
+ match '/about', to: 'static_pages#about', via: 'get'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
