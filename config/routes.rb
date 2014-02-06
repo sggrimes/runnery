114d4652b628df01c88550cdcd207cc2f6af2574
Sam <Samuel.g.grimes@gmail.com>
2014-02-06 12:40:52 -0600

    Fix user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Runnery::Application.routes.draw do
- #resources :users do
-  #member do
-    #get "feed"
-  #end
-#end
 
- resources :users, only: [:edit, :update, :create, :destroy] 
+ resources :users
  resources :sessions, only: [:new, :create, :destroy]
  resources :orders, only: [:create,:destroy,:update]
 
