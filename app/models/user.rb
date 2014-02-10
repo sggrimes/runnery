357ee84f5e2f5eeb986f32a4e13a6761c3c35e62
Sam <Samuel.g.grimes@gmail.com>
2014-02-10 13:43:55 -0600

    Remove faulty ip method

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,9 +9,6 @@ class User < ActiveRecord::Base
 	has_secure_password
 	validates :password, length: { minimum:6 }
 
-  geocoded_by :ip_address
-  after_validation :geocode
-
 
 
   def restaurant_wait
