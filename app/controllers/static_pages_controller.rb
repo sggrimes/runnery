bbfc8ffe7102d44800a2ae942f386a2937e50d88
Sam <Samuel.g.grimes@gmail.com>
2014-02-12 16:19:09 -0600

    request locations without dev

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,8 +7,11 @@ class StaticPagesController < ApplicationController
       @feed_items = current_user.restaurant_wait.paginate(page: params[:page], :per_page => 3)
       if driver?
       @feed_items = current_user.driver_wait.paginate(page: params[:page], :per_page => 3)
-                  
-      
+     
+      @lat = request.location.latitude
+      @long = request.location.longitude
+      @locations = @feed_items.near([@lat, @long], 1)
+
       @hash = Gmaps4rails.build_markers(@feed_items) do |feed_item, marker|
         
         marker.lat feed_item.origin_latitude
