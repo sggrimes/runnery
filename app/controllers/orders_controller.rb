5f47f9f1892cc72cfa6b8e8a63299868cbbdd124
Sam <Samuel.g.grimes@gmail.com>
2014-03-06 14:23:06 -0600

    Set driver claim run priority

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,32 +1,22 @@
 class OrdersController < ApplicationController
-  #before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :signed_in_user, only: [:create, :destroy, :update]
   before_action :correct_user,   only: :destroy
 
-  # GET /orders
-  # GET /orders.json
   def index
     @orders = Order.all
   end
 
-  # GET /orders/1
-  # GET /orders/1.json
   def show 
     @order = Order.find_by(params[:id])
-end
+  end
 
-
-  # GET /orders/new
   def new
     @order = Order.new
   end
 
-  # GET /orders/1/edit
   def edit
   end
 
-  # POST /orders
-  # POST /orders.json
   def create
     @order = current_user.orders.build(order_params)
     if @order.save
@@ -37,59 +27,39 @@ end
       render 'static_pages/home'
     end
 
-    #respond_to do |format|
-     # if @order.save
-      
-      #  format.html { redirect_to @order, notice: 'Order was successfully created.' }
-       # format.json { render action: 'show', status: :created, location: @order }
-      #else
-       # format.html { render action: 'new' }
-        #format.json { render json: @order.errors, status: :unprocessable_entity }
-      #end
-    #end
   end
 
-  # PATCH/PUT /orders/1
-  # PATCH/PUT /orders/1.json
   def update
     @feed_item = Order.find(params[:id])
     if !@feed_item.running
      if @feed_item.update(order_params)
         flash[:success] = "Order Running!"
-      redirect_to running_path
-    end
+        redirect_to running_path
+     end
     else
-      if @feed_item.update(order_params)
-        if @feed_item.driver_id != nil
+     if @feed_item.update(order_params)
+      if !@feed_item.receipt.nil?
         flash[:success] = "Order Done!"
-      redirect_to done_path
-    else
-      flash[:error] = "Order stopped!"
-      redirect_to running_path
+        redirect_to done_path 
+      else
+      if @feed_item.driver_id.nil?
+        flash[:error] = "Order stopped!"
+        redirect_to running_path
+      else
+      redirect_to root_url
+      end
+      end
     end
     end
   end
-end
 
-  # DELETE /orders/1
-  # DELETE /orders/1.json
   def destroy
     @order.destroy
     redirect_to root_url
-   # respond_to do |format|
-      #format.html { redirect_to orders_url }
-      #format.json { head :no_content }
-    #end
   end
   
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    #def set_order
-      #@order = Order.find(params[:id])
-    #end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
     def order_params
       params.require(:order).permit(:address, :phone, :surcharge, :user_id, :pay_type, :driver_id, :receipt, :redeemed)
     end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,17 +1,11 @@
 class UsersController < ApplicationController
-# before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-  #before_action :admin_user,    only: :destroy
 
-  # GET /users
-  # GET /users.json
   def index
     @users = User.paginate(page: params[:page])
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show
     @user = User.find(params[:id])
   end
@@ -49,7 +43,7 @@ class UsersController < ApplicationController
     end
   end
 
- def all_done
+  def all_done
   @user = User.find_by(params[:id])
 
    if driver?
@@ -58,20 +52,16 @@ class UsersController < ApplicationController
     @feed_items = current_user.restaurant_all_done.paginate(page: params[:page], :per_page => 10).reorder("created_at DESC")
    end
    
- end
+  end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # GET /users/1/edit
   def edit
     @user = User.find(params[:id])
   end
 
-  # POST /users
-  # POST /users.json
   def create
    @user = User.new(user_params)
     if @user.save
@@ -83,40 +73,22 @@ class UsersController < ApplicationController
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
-    respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'Register updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: 'edit' }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
-    end
+    @user = User.find(params[:id])
+     if @user.update(user_params)
+     flash[:success] = "User Register Updated!"
+     end
+    redirect_to @user
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     User.find(params[:id]).destroy
-    flash[:success] = "User deleted."
+    flash[:error] = "User deleted."
     redirect_to root_url
-
-    #respond_to do |format|
-      #format.json { head :no_content }
-    #end
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
-  # def set_user
-  #  @user = User.find(params[:id])
-  # end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-    
     def user_params
       params.require(:user).permit(:name, :phone, :address, :user_type, :password, :password_confirmation)
     end
